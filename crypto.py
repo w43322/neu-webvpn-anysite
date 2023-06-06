c85d8974cb86e74317c6e712494b97b644a40e39
w43322 <raywang777@foxmail.com>
2023-06-06 16:12:02 +0800

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -3,12 +3,12 @@ from Crypto.Cipher import AES
 url = 'neucsecg.neu.edu.cn'
 
 cipher = AES.new(
-    'wrdvpnisthebest!',
+    'b0A58a69394ce73@',
     AES.MODE_CFB,
-    'wrdvpnisthebest!',
+    'b0A58a69394ce73@',
     segment_size=128)
 cipher_text = cipher.encrypt(url.ljust(len(url)//16*16+16, '\0').encode())
 
-res = 'https://webvpn.neu.edu.cn/http/77726476706e69737468656265737421' \
+res = 'https://webvpn.neu.edu.cn/http/62304135386136393339346365373340' \
     + cipher_text[:len(url)].hex()
 print(res)
